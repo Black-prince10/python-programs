fc03448f9ca9e007843ddb418723824232010c06
Black-prince10 <56605843+Black-prince10@users.noreply.github.com>
2021-03-18 19:09:06 +0100

    fixing problem

--- Python Program to Print all Prime Numbers between an Interval.py
+++ Python Program to Print all Prime Numbers between an Interval.py
@@ -2,16 +2,21 @@
 We have already read the concept of prime numbers in the previous program. 
 Here, we are going to print the prime numbers between given interval.
 '''
-lower =int(input("enter lower range:"))
-upper =int(input("enter upper range:"))
+lower = int(input('Enter lower range: '))
+upper = int(input('Enter upper range: '))
 
-for num in range(lower,upper+1):
-	if num > 1:
-		for i in range(2,num):
-			if num%i == 0:
-				break 
-		else:
-			print(num)	
+
+for num in range(lower,upper+1): 
+    if num > 1: 
+        
+        for i in range(2,num//2):
+            if num%i ==0:
+                ok = False
+                break
+            else: 
+                ok=True
+    if ok == True : 
+        print(num)	
 
 
 
